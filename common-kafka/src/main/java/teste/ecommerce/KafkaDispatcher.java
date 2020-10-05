460aa15b0fd55bbe1c610f34ea3586dfb8dc4789
Marcos Henrique <contato@marcoshsilva.com.br>
2020-10-05 17:46:41 -0300

    feat(Report): inserir um modulo novo de criacao de relatorio e comunicacao com servico de banco.

--- common-kafka/src/main/java/teste/ecommerce/KafkaDispatcher.java
+++ common-kafka/src/main/java/teste/ecommerce/KafkaDispatcher.java
@@ -6,6 +6,7 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
+import javax.swing.*;
 import java.io.Closeable;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
@@ -23,6 +24,7 @@ public class KafkaDispatcher<T> implements Closeable {
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, GsonSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GsonSerializer.class.getName());
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
         return properties;
     }
 
